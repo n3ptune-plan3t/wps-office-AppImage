33e0f87d75ca757b284df0814115dc0d58e9873e
n3ptune-plan3t <plawanjyotitalukdar@gmail.com>
2026-07-05 18:19:27 +0000

    Enhance make-appimage.sh with post-install fixes
    Added post-install fixes to disable background processes and ensure English environment by removing Chinese language files.

--- make-appimage.sh
+++ make-appimage.sh
@@ -5,14 +5,31 @@ set -eu
 ARCH=$(uname -m)
 VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
+export APPDIR="./AppDir"
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.hook"
+mkdir -p "$APPDIR/usr/bin" "$APPDIR/usr/lib" "$OUTPATH"
+#export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+
+# Disable background processes to prevent shutdown crashes
+echo "Applying post-install fixes..."
+chmod -x /usr/lib/office6/wpscloudsvr
+chmod -x /usr/lib/office6/wpsoffice
+
+# Force English environment and strip Chinese resources
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+# Remove Chinese language files to ensure English fallback
+if [ -d "/usr/lib/office6/mui/zh_CN" ]; then
+    rm -rf "/usr/lib/office6/mui/zh_CN"
+fi
+
+export ICON="/usr/share/icons/hicolor/scalable/apps/wps-office-wpsmain.svg"
+export DESKTOP="/usr/share/applications/wps-office-wps.desktop"
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/wps
 
 # Additional changes can be done in between here
 
